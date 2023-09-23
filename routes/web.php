fa298238dd01ceaf0a9f61050179b723acab09c0
Sergey119 <manymail08@gmail.com>
2023-09-23 09:22:52 +0300

    my changes

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\PostController@index');
 
-Route::get('posts/', 'App\Http\Controllers\PostController@index')->name('post.index');
+Route::get('posts/', 'App\Http\Controllers\PostController@index')->name('posts.index');
 
-Route::get('posts/create', 'App\Http\Controllers\PostController@create')->name('post.create');
+Route::get('posts/create', 'App\Http\Controllers\PostController@create')->name('posts.create');
+
+Route::post('posts/', 'App\Http\Controllers\PostController@store')->name('posts.store');
 
 //Auth::routes();
 //
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
